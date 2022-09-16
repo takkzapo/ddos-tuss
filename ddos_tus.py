9031d846344fbce0fc0890decac6d225ab762ac4
takkzapo <113465413+takkzapo@users.noreply.github.com>
2022-09-16 06:56:23 +0000

    Update ddos_tus.py

--- ddos_tus.py
+++ ddos_tus.py
@@ -39,7 +39,7 @@ def menu():
     clear()
     print('HANDOS By LeMinhTu [LeMinhTu.info] ')
     print("https://LeMinhTu.info")
-    print(Colorate.Horizontal(Colors.red_to_purple, + """
+    print(Center.XCenter(Colorate.Vertical(Colors.red_to_purple, + """
             ╚═════════════════════╦═════════════════════════════════════════╦══════════════════════╝
                 ╔═════════════════╩══════════════[HANDOS-DDoS]══════════════╩══════════════════╗
                ╔╝------------------------------------------------------------------------------╚╗
